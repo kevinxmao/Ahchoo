0cd60c098a5fb10387ce4c1a39f44d74d322c25a
Kevin <kevinmao.xy@gmail.com>
2021-03-15 16:00:53 -0400

    make current_user persist

--- frontend/ahchoo.jsx
+++ frontend/ahchoo.jsx
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+// test import
+import { login, logout } from './actions/session_actions';
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    const store = configureStore();
+    let store;
+
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: {id: window.currentUser.id}
+        }
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+    // test
+    window.store = store;
+    window.login = login;
+    window.logout = logout;
 
     ReactDOM.render(<Root store={store}/>, root);
 });
