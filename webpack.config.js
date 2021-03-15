4bc448dd9d01652b31d73f41b0bc0eebbc09e5df
Kevin <kevinmao.xy@gmail.com>
2021-03-15 08:47:39 -0400

    setup npm react

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./frontend/ahchoo.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+};
