15fdd4d6d795a7d3485296b215fd1d7ef4a1303f
Kevin <kevinmao.xy@gmail.com>
2021-03-15 21:58:44 -0400

    Add login route to App.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import LoginForm from "./forms/login_form_container";
 
 const App = () => (
     <div>
         <header>
             <h1>Ahchoo is Live!</h1>
         </header>
-        
+        <Route path='/login' exact component={LoginForm} />
     </div>
 )
 
