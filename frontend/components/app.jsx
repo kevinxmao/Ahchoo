d7b3c372e488b744ed1ded01d3d6702d4d77a480
Kevin <kevinmao.xy@gmail.com>
2021-03-15 23:30:56 -0400

    Make signup form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import LoginForm from "./forms/login_form_container";
+import SignupForm from "./forms/signup_form_container";
 
 const App = () => (
     <div>
@@ -8,6 +9,8 @@ const App = () => (
             <h1>Ahchoo is Live!</h1>
         </header>
         <Route path='/login' exact component={LoginForm} />
+        <Route path='/signup' exact component={SignupForm} />
+
     </div>
 )
 
--- frontend/components/forms/signup_form.jsx
+++ frontend/components/forms/signup_form.jsx
@@ -1,9 +1,64 @@
 import React from "react";
 
 class SignupForm extends React.Component {
-  render() {
-    return null;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(field) {
+        return (e) => this.setState({ [field]: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.signup(this.state);
+    }
+
+    render() {
+        return (
+            <div id="signup-form">
+                <p>Make it rain</p>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        value={this.state.firstName}
+                        onChange={this.handleChange("firstName")}
+                    />
+                    <br/>
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        value={this.state.lastName}
+                        onChange={this.handleChange("lastName")}
+                    />
+                    <br/>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleChange("email")}
+                    />
+                    <br/>
+                    <input
+                        type="password"
+                        placeholder="Password (min. 8 characters)"
+                        value={this.state.password}
+                        onChange={this.handleChange("password")}
+                    />
+                    <br/>
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SignupForm;
