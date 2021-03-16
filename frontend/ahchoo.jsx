010a3ff3a11d1a9f9c56c8b69494ae2c1387d092
Kevin <kevinmao.xy@gmail.com>
2021-03-16 12:58:11 -0400

    Add logo and fonts

--- frontend/ahchoo.jsx
+++ frontend/ahchoo.jsx
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             session: {id: window.currentUser.id}
         }
+        delete window.currentUser;
         store = configureStore(preloadedState);
     } else {
         store = configureStore();
--- frontend/components/splash/splash_nav.jsx
+++ frontend/components/splash/splash_nav.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 class SplashNav extends React.Component {
     constructor(props) {
         super(props);
@@ -8,24 +7,36 @@ class SplashNav extends React.Component {
     
     render() {
         return (
-            <nav id="splash-nav">
-                <div id="splash-left">
-                    <div id="splah-nav-logo">
-                        <p>Ahchoo</p>
-                    </div>
-                    <ul id="splash-nav-list">
-                        <li>Products</li>
-                        <li>Learn</li>
-                        <li>Support</li>
-                        <li>About</li>
-                    </ul>
-                </div>
-                <div id="splash-nav-session">
-                    <NavLink to="/login" className="button-login" activeClassName="selected">Log In</NavLink>
-                    <NavLink to="/signup" className="button-signup" activeClassName="selected">Sign Up</NavLink>
-                </div>
-            </nav>
-        )
+          <nav id="splash-nav">
+            <div id="splash-left">
+              <div id="splah-nav-logo">
+                <img src={window.textLogo} alt="logo" />
+              </div>
+              <ul id="splash-nav-list">
+                <li>Products</li>
+                <li>Learn</li>
+                <li>Support</li>
+                <li>About</li>
+              </ul>
+            </div>
+            <div id="splash-nav-session">
+              <NavLink
+                to="/login"
+                className="button-login"
+                activeClassName="selected"
+              >
+                Log In
+              </NavLink>
+              <NavLink
+                to="/signup"
+                className="button-signup"
+                activeClassName="selected"
+              >
+                Sign Up
+              </NavLink>
+            </div>
+          </nav>
+        );
     }
 }
 
