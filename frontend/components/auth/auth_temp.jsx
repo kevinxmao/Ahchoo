2623d15e1f47e25e3ad06df0670fbb9cccb81b3c
Kevin <kevinmao.xy@gmail.com>
2021-03-17 17:18:35 -0400

    Make video auth

--- frontend/components/auth/auth_temp.jsx
+++ frontend/components/auth/auth_temp.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 // place holder for log out
-const AuthTemp = props => (
-    <button onClick={() => props.logout().then(
-        () => {
-                delete window.currentUser;
-                <Redirect to="/" />
-            }
-        )}
+const AuthTemp = (props) => (
+  <div style={{ width: "800px", margin: "auto" }}>
+    <button
+      style={{ "margin-bottom": "30px" }}
+      onClick={() =>
+        props.logout().then(() => {
+          delete window.currentUser;
+          <Redirect to="/" />;
+        })
+      }
     >
-        Log Out
+      Log Out
     </button>
-)
+    <p>Coming soon...</p>
+    <p style={{ "margin-bottom": "30px" }}>
+      In the mean time, enjoy this video of cat vibing to Polka
+    </p>
+    <ReactPlayer
+      url="https://www.youtube.com/watch?v=NUYvbT6vTPs"
+      muted={true}
+      controls={true}
+    />
+  </div>
+);
 
 export default AuthTemp;
