388239f60dd64790ea235c8a1c89f94ba8bd7a25
Kevin <kevinmao.xy@gmail.com>
2021-03-17 09:45:11 -0400

    Create signup page containing form container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import LoginFormContainer from "./forms/login_form_container";
-import SignupFormContainer from "./forms/signup_form_container";
+// import LoginFormContainer from "./forms/login_form_container";
+// import SignupFormContainer from "./forms/signup_form_container";
 import AuthNavContainer from "./auth/auth_nav_container";
 import Splash from "./splash/splash";
 import { AuthRoute, ProtectedRoute } from "../util/auth/routes_util";
 import LoginPage from "./login/login_page";
+import SignupPage from "./signup/signup_page";
 
 const App = () => (
     <div>
         <Switch>
             <Route exact path='/' component={Splash} />
             <AuthRoute path='/login' component={LoginPage} />
-            <AuthRoute path='/signup' component={SignupFormContainer} />
+            <AuthRoute path='/signup' component={SignupPage} />
         </Switch>
         <ProtectedRoute path="/auth" component={AuthNavContainer} />
     </div>
---
+++ frontend/components/signup/signup_page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignupFormContainer from "../forms/signup_form_container";
+
+class SignupPage extends React.Component {
+    render() {
+        return (
+            <div id="signup-page">
+                <SignupFormContainer />
+            </div>
+        )
+    }
+}
+
+export default SignupPage;
