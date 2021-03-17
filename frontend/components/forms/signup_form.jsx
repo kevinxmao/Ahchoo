b0c8b9194f7138dc2d09ccdc029c628430585864
Kevin <kevinmao.xy@gmail.com>
2021-03-17 10:46:27 -0400

    Make reusable sass variables

--- frontend/components/forms/signup_form.jsx
+++ frontend/components/forms/signup_form.jsx
@@ -23,41 +23,51 @@ class SignupForm extends React.Component {
     }
 
     render() {
-        // if (window.currentUser) this.props.history.push("/");
 
         return (
             <div id="signup-form">
-                <p>Make it rain</p>
                 <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="First Name"
-                        value={this.state.firstName}
-                        onChange={this.handleChange("firstName")}
-                    />
-                    <br/>
-                    <input
-                        type="text"
-                        placeholder="Last Name"
-                        value={this.state.lastName}
-                        onChange={this.handleChange("lastName")}
-                    />
-                    <br/>
-                    <input
-                        type="text"
-                        placeholder="Email"
-                        value={this.state.email}
-                        onChange={this.handleChange("email")}
-                    />
-                    <br/>
-                    <input
-                        type="password"
-                        placeholder="Password (min. 8 characters)"
-                        value={this.state.password}
-                        onChange={this.handleChange("password")}
-                    />
-                    <br/>
-                    <button type="submit">Sign Up</button>
+                    <header className="signup-form-header">
+                        <div className="signup-welcome-message">Make it rain</div>
+                        <div className="signup-welcome-subtitle">Ahchoo lets you invest in companies you love, commission-free.</div>
+                    </header>
+
+                    <div className="signup-main">
+                        <div className="signup-form-1">
+                            <input
+                                type="text"
+                                placeholder="First name"
+                                value={this.state.firstName}
+                                onChange={this.handleChange("firstName")}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Last name"
+                                value={this.state.lastName}
+                                onChange={this.handleChange("lastName")}
+                            />
+                        </div>
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            value={this.state.email}
+                            onChange={this.handleChange("email")}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password (min. 8 characters)"
+                            value={this.state.password}
+                            onChange={this.handleChange("password")}
+                        />
+                    </div>
+
+                    <footer className="signup-footer">
+                        <div className="signup-button-container">
+                            <button type="submit">
+                                <span>Sign Up</span>
+                            </button>
+                        </div>
+                    </footer>
                 </form>
                 <FormError errors={this.props.errors}/>
             </div>
