f037b5fc1a2d2ad468f233974133b379ddadd168
Kevin <kevinmao.xy@gmail.com>
2021-03-17 11:26:30 -0400

    Create demo login util

---
+++ frontend/util/auth/demo_user.jsx
@@ -0,0 +1,15 @@
+import { login } from "../../actions/session_actions";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+const Demo = props => (
+    <button className="demo-login" onClick={() => props.login({email: "demo@app.com", password="demouser"})}>
+        <span>Demo Login</span>
+    </button>
+)
+
+const mapDispatchToProps = dispatch => ({
+    login: (user) => dispatch(login(user))
+});
+
+export const DemoLogin = withRouter(connect(null, mapDispatchToProps)(Demo));
