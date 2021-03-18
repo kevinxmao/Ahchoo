44a4ec042d768026df35781d92d4a3d435932d07
Kevin <kevinmao.xy@gmail.com>
2021-03-18 14:33:35 -0400

    Add holdings backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:show, :create, :edit]
     resource :session, only: [:create, :destroy]
+    resources :users, only: [:show, :create, :edit]
+    resources :holdings, only: [:index, :show, :create, :edit, :destroy]
   end
 end
