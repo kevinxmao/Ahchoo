d490fcf11a4b116fc25a5baf155c5f1efc92c2e7
Kevin <kevinmao.xy@gmail.com>
2021-03-18 12:13:32 -0400

    Create seeding of tickers table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'csv'
+
 User.delete_all
+Ticker.delete_all
 
 User.create({email: 'demo@app.com', first_name: 'Demo', last_name: 'User', password: 'demouser'});
+
+# Seed tickers table
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'listings.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    if row['Company'].include?('Common Stock')
+        t = Ticker.new
+        t.ticker = row['Ticker']
+        t.company = row['Company'].gsub(/ Common Stock/, "")
+        t.save
+        puts "#{t.ticker}, #{t.company} saved"
+    end
+end
