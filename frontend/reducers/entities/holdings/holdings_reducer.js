ff3afbe3e55b312e5151b7773c304b029468bd99
Kevin <kevinmao.xy@gmail.com>
2021-03-19 11:57:59 -0400

    Update holdings_reducer.js

--- frontend/reducers/entities/holdings/holdings_reducer.js
+++ frontend/reducers/entities/holdings/holdings_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_USER } from "../../../actions/users_actions";
+import { RECEIVE_HOLDINGS, RECEIVE_HOLDING, REMOVE_HOLDING } from "../../../actions/holdings_action";
 
 const holdingsReducer = (state={}, action) => {
     Object.freeze(state);
@@ -6,6 +7,13 @@ const holdingsReducer = (state={}, action) => {
     switch(action.type) {
         case RECEIVE_USER:
             return action.payload.holdings;
+        case RECEIVE_HOLDINGS:
+            return action.holdings;
+        case RECEIVE_HOLDING:
+            return Object.assign({}, newState, {[action.holding.id]: action.holding})
+        case REMOVE_HOLDING:
+            delete newState[action.holdingId];
+            return newState;
         default:
             return newState;
     }
