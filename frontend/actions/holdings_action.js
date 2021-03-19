f371c620f2e74b65fe7e3b19f9818673d22a3165
Kevin <kevinmao.xy@gmail.com>
2021-03-19 11:30:51 -0400

    create holdings action creator

--- frontend/actions/holdings_action.js
+++ frontend/actions/holdings_action.js
@@ -1,5 +1,44 @@
+import * as HoldingsAPIUtil from '../util/holdings/holdings_api_util'
+
 export const RECEIVE_HOLDINGS = 'RECEIVE_HOLDINGS';
 export const RECEIVE_HOLDING = 'RECEIVE_HOLDING';
 export const REMOVE_HOLDING = 'REMOVE_HOLDINGD';
 
-export 
+export const receiveHolding = holding => ({
+    type: RECEIVE_HOLDING,
+    holding
+})
+
+export const receiveHoldings = holdings => ({
+    type: RECEIVE_HOLDINGS,
+    holdings
+})
+
+export const removeHolding = holdingId => ({
+    type: REMOVE_HOLDING,
+    holdingId
+})
+
+export const fetchHoldings = () => dispatch => (
+    HoldingsAPIUtil.fetchHoldings().then(
+        holdings => dispatch(receiveHoldings(holdings))
+    )
+);
+
+export const fetchHolding = holdingId => dispatch => (
+    HoldingsAPIUtil.fetchHolding(holdingId).then(
+        holding => dispatch(receiveHolding(holding))
+    )
+);
+
+export const createHolding = holding => dispatch => (
+    HoldingsAPIUtil.createHolding(holding).then(
+        holding => dispatch(receiveHolding(holding))
+    )
+);
+
+export const removeHolding = holdingId => dispatch => (
+    HoldingsAPIUtil.removeHolding(holdingId).then(
+        () => dispatch(removeHolding(holdingId))
+    )
+);
