6862770d6188d5877d9fe87679c14a206e8fef56
Kevin <kevinmao.xy@gmail.com>
2021-03-22 15:45:43 -0400

    Configure production api key storage

--- frontend/util/companies/data_api_util.js
+++ frontend/util/companies/data_api_util.js
@@ -1,10 +1,21 @@
 import { TD_ACCESS_TOKEN, TD_API_KEY } from "../../../secret";
 
+let apiKey;
+let accessToken;
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  apiKey = TD_API_KEY;
+  accessToken = TD_ACCESS_TOKEN;
+} else {
+  apiKey = process.env.TD_API_KEY;
+  accessToken = process.env.TD_ACCESS_TOKEN;
+}
+
 export const fetchSingleQuote = (ticker) =>
   $.ajax({
     method: "GET",
-    url: `https://api.tdameritrade.com/v1/marketdata/${ticker}/quotes?apikey=${TD_API_KEY}`,
+    url: `https://api.tdameritrade.com/v1/marketdata/${ticker}/quotes?apikey=${apiKey}`,
     headers: {
-      "Authorization": `Bearer ${TD_ACCESS_TOKEN}`,
+      "Authorization": `Bearer ${accessToken}`,
     },
   });
