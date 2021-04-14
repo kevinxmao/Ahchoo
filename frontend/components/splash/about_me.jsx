07ff4bc168501431b7c3cbc6efe198e196cf8af9
Kevin <kevinmao.xy@gmail.com>
2021-04-14 16:16:01 -0400

    Style about me modal

--- frontend/components/splash/about_me.jsx
+++ frontend/components/splash/about_me.jsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { closeModal, openModal } from "../../actions/modal_actions";
 import { connect } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/pro-regular-svg-icons';
 
-export default function AboutMe() {
+function AboutMe(props) {
     return (
         <div className="about-me">
+            <header className="modal-title">
+                <div></div>
+                <button onClick={() => props.closeModal()}>
+                    <span><FontAwesomeIcon icon={faTimes}/></span>
+                </button>
+            </header>
             <div className='avatar-container'>
-                <img src="" alt=""/>
+                <img src={window.avatar} alt=""/>
             </div>
             <div className='developer'>
                 <span>Kevin Mao</span>
             </div>
             <div className="description">
-                <p>Hi, I am a fullstack software engineer. I graduated from App Academy in April 2021. I built Ahchoo so you can enjoy buying and selling stocks with virtual money.</p>
+                <p>Hello, I am a fullstack software engineer. I graduated from App Academy in April 2021. I built Ahchoo so you can enjoy buying and selling stocks with virtual money.</p>
             </div>
         </div>
     )
 }
+
+const mDTP = dispatch => ({
+    closeModal: () => dispatch(closeModal())
+})
+
+export default connect(null, mDTP)(AboutMe);
