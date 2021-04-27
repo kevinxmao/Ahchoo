c4cbf66daa915914d0bec8bcccdebb1257276f56
Kevin <kevinmao.xy@gmail.com>
2021-04-27 15:38:10 -0400

    Create watchlist fetch actions

--- frontend/actions/watchlists_actions.js
+++ frontend/actions/watchlists_actions.js
@@ -3,4 +3,14 @@ import * as WatchlistsAPIUtil from '../util/watchlists/watchlists_api_util';
 export const RECEIVE_WATCHLISTS = 'RECEIVE_WATCHLISTS';
 export const RECEIVE_WATCHLIST = 'RECEIVE_WATCHLIST';
 export const UPDATE_WATCHLIST = 'UPDATE_WATCHLIST';
-export const REMOVE_WATCHLIST = 'REMOVE_WATCHLIST';w
+export const REMOVE_WATCHLIST = 'REMOVE_WATCHLIST';
+
+export const receiveWatchlists = watchlists => ({
+    type: RECEIVE_WATCHLISTS,
+    watchlists
+})
+
+export const receiveWatchlist = watchlist => ({
+    type: REMOVE_WATCHLIST,
+    watchlist
+})
