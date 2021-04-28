60be3e1c41988852f2add04cb1e90e0d60766c63
Kevin <kevinmao.xy@gmail.com>
2021-04-27 22:59:22 -0400

    Update watchlists_actions.js

--- frontend/actions/watchlists_actions.js
+++ frontend/actions/watchlists_actions.js
@@ -4,6 +4,7 @@ export const RECEIVE_WATCHLISTS = 'RECEIVE_WATCHLISTS';
 export const RECEIVE_WATCHLIST = 'RECEIVE_WATCHLIST';
 export const UPDATE_WATCHLIST = 'UPDATE_WATCHLIST';
 export const REMOVE_WATCHLIST = 'REMOVE_WATCHLIST';
+export const RECEIVE_WATCHLIST_ERRORS = 'RECEIVE_WATCHLIST_ERRORS';
 
 export const receiveWatchlists = watchlists => ({
     type: RECEIVE_WATCHLISTS,
@@ -14,3 +15,20 @@ export const receiveWatchlist = watchlist => ({
     type: REMOVE_WATCHLIST,
     watchlist
 })
+
+export const receiveWatchlistErrors = (errors) => ({
+    type: RECEIVE_WATCHLIST_ERRORS,
+    watchlist: errors
+})
+
+export const fetchWatchlists = () => (
+    WatchlistsAPIUtil.fetchWatchlists().then(
+        watchlists => dispatchEvent(receiveWatchlists(watchlists))
+    )
+)
+
+export const fetchWatchlist = (id) => (
+    WatchlistsAPIUtil.fetchWatchlist(id).then(
+        watchlist => dispatchEvent(receiveWatchlist(watchlist))
+    )
+)
