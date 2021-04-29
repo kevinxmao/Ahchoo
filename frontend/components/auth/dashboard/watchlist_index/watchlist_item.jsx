b519eff6c47df0ed54e07c3422e61c6daf64bef4
Kevin <kevinmao.xy@gmail.com>
2021-04-29 16:08:11 -0400

    Update watchlist sidebar style

---
+++ frontend/components/auth/dashboard/watchlist_index/watchlist_item.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ItemChart from "../holdings_index/item_chart";
+import { formatNumber, formatPercent } from "../../../../util/util_functions";
+
+export default function WatchlistItem(props) {
+    const color = change >= 0 ? "#00c807" : "#ff5000";
+
+    return (
+        <Link to={`/auth/tickers/${ticker}`} className="sidebar-list-item">
+            <div className="ticker-symbol">
+                <span>{ticker}</span>
+            </div>
+            <div className="list-item-chart">
+                <ItemChart referenceValue={open} data={chartData} change={change} />
+            </div>
+            <div className="holding-price">
+                <div className="list-current-price">
+                    <span>{`${formatNumber(market)}`}</span>
+                </div>
+                <div className="list-percent-change">
+                    <span style={{ color: `${color}` }}>
+                        {percentChange >= 0
+                            ? `+${formatPercent(percentChange)}`
+                            : `-${formatPercent(percentChange)}`}
+                    </span>
+                </div>
+            </div>
+        </Link>
+    )
+}
