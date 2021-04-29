b208ad59b50f2bc769bfcb84ac3fcde836cea5fb
Kevin <kevinmao.xy@gmail.com>
2021-04-29 16:38:13 -0400

    create watchlist item hook

--- frontend/components/auth/dashboard/watchlist_index/watchlist.jsx
+++ frontend/components/auth/dashboard/watchlist_index/watchlist.jsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown, faEllipsisH } from '@fortawesome/pro-regular-svg-icons';
+import WatchlistItem from './watchlist_item';
+import { fetchAllQuotes } from '../../../../util/companies/data_api_util';
 
 export default function Watchlist(props) {
     const downArrow = <FontAwesomeIcon icon={faAngleDown} />;
     const upArrow = <FontAwesomeIcon icon={faAngleUp} />
     const ellipsis = <FontAwesomeIcon icon={faEllipsisH}/>
-    const [expand, setExpand] = useState(false);
+    const [expand, setExpand] = useState(true);
+    const [data, setData] = useState(null);
 
     function handleExpandClick() {
         setExpand(!expand)
     }
 
     function expandedList() {
-        return;
+        return props.watchlist.tickers.map(
+            item => <WatchlistItem key={item.id} ticker={item.ticker} datum={data[item.ticker]}/>
+        )
     }
+
+    useEffect(() => {
+        const tickers = props.watchlist.tickers.map(item => item.ticker);
+        fetchAllQuotes(tickers).then(quotes => setData(quotes));
+    }, [])
     
     return (
         <div className="watchlist">
@@ -27,6 +37,7 @@ export default function Watchlist(props) {
                     </div>
                 </header>
             </div>
+            {(data && expand) && <div className="list-items">{expandedList()}</div>}
         </div>
     )
 }
--- frontend/components/auth/dashboard/watchlist_index/watchlist_item.jsx
+++ frontend/components/auth/dashboard/watchlist_index/watchlist_item.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import ItemChart from "../holdings_index/item_chart";
 import { formatNumber, formatPercent } from "../../../../util/util_functions";
 
 export default function WatchlistItem(props) {
     const color = change >= 0 ? "#00c807" : "#ff5000";
+
+    useEffect(() => {
+        
+    })
 
     return (
         <Link to={`/auth/tickers/${ticker}`} className="sidebar-list-item">
