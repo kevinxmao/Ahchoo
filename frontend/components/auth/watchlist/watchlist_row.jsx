614f2bd790529af800af4d580a566d00e2cc5c68
Kevin <kevinmao.xy@gmail.com>
2021-05-04 11:19:19 -0400

    Style price change

--- frontend/components/auth/watchlist/watchlist_row.jsx
+++ frontend/components/auth/watchlist/watchlist_row.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/pro-regular-svg-icons';
+import { faTriangle } from '@fortawesome/pro-solid-svg-icons';
+// import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import { formatNumber, formatLargeNumber, formatPercent } from '../../../util/util_functions';
+import { useDispatch } from 'react-redux';
 
 export default function WatchlistRow(props) {
     const {name, symbol, price, today, marketCap} = props.data[props.symbol];
+    const down = <FontAwesomeIcon icon={faTriangle} color="#ff5000" size="xs" rotation={180}/>
+    const up = <FontAwesomeIcon icon={faTriangle} color="#00c807" size="xs" />
+    const dispatch = useDispatch();
 
     function handleClick() {
-        
+        const watchlist = props.watchlist;
+        const newTickers = watchlist.tickers.filter(ticker => ticker.ticker !== symbol);
+        console.log(newTickers);
     }
 
     return (
@@ -25,7 +33,7 @@ export default function WatchlistRow(props) {
                         <span>{formatNumber(price)}</span>
                     </div>
                     <div className="table-cell">
-                        <span>{formatPercent(today)}</span>
+                        <span><div>{today > 0 ? up : down}</div> {formatPercent(today)}</span>
                     </div>
                     <div className="table-cell">
                         <span>{formatLargeNumber(marketCap)}</span>
@@ -33,7 +41,7 @@ export default function WatchlistRow(props) {
                 </div>
             </Link>
             <div className="item-delete-icon">
-                <button className="btn delete-watchlist-item">
+                <button className="btn delete-watchlist-item" onClick={handleClick}>
                     <FontAwesomeIcon icon={faTimes} />
                 </button>
             </div>
