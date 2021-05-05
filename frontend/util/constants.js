a8f00452adffddd33639cb7facd872c401a10669
Kevin <kevinmao.xy@gmail.com>
2021-05-05 17:20:20 -0400

    create color constants

---
+++ frontend/util/constants.js
@@ -0,0 +1,13 @@
+export const GREEN = {
+    base: "rgb(0, 200, 5)",
+    hover: "rgba(0, 180, 5, 1)",
+    pressed: "rgba(0, 160, 4, 1)",
+    light: "rgb(0, 200, 5, 0.1)"
+}
+
+export const RED = {
+    base: "rgb(255, 80, 0)",
+    hover: "rgba(230, 72, 0, 1)",
+    pressed: "rgba(204, 64, 0, 1)",
+    light: "rgba(255, 80, 0, 0.1)"
+}
