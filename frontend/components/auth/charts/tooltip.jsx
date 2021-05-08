f6e3ba8d8c8bebb5479121ba7ced61a38825df88
Kevin <kevinmao.xy@gmail.com>
2021-05-07 20:55:40 -0400

    Update tooltip.jsx

--- frontend/components/auth/charts/tooltip.jsx
+++ frontend/components/auth/charts/tooltip.jsx
@@ -3,25 +3,36 @@ import ReactDOM from 'react-dom';
 import { formatNumber } from "../../../util/util_functions";
 
 const CustomToolTip = ({active, payload, label, referenceValue}) => {
+    // const prevSumEle = document.getElementById("_sum");
+    // const prevChangeEle = document.getElementById("_change");
+    // const prevPercentChangeEle = document.getElementById("_percentChange");
     
     if (active && payload && payload.length) {
-        const prevSum = document.getElementById("_sum").innerHTML;
-        const prevChange = document.getElementById("_change").innerHTML;
-        const prevPercentChange = document.getElementById("_percentChange").innerHTML;
 
-        const change = payload[0].payload.value - referenceValue;
-        const percentChange = change / referenceValue;
+        // const change = payload[0].payload.value - referenceValue;
+        // const percentChange = change / referenceValue;
 
-        ReactDOM.render(<>{formatNumber(payload[0].payload.value)}</>, document.getElementById("_sum"));
+        // ReactDOM.render(<>{formatNumber(payload[0].payload.value)}</>, document.getElementById("_sum"));
 
         return (
             <div className="custom-tooltip">
                 <p>Hello</p>
             </div>
         )
-    }
+    } else if (!active) {
+        // let prevSum, prevChange, prevPercentChange;
 
+        // prevChange = prevChangeEle.innerHTML || "",
+        // prevPercentChange = prevPercentChangeEle.innerHTML || "";
+        console.log(payload);
+
+        // if (prevSum) {
+        //     prevSum = prevSumEle.innerHTML;
+        //     ReactDOM.render(<>{prevSumEle}</>, document.getElementById("_sum"));
+        // }
+    }
     return null;
+
 }
 
 export default CustomToolTip;
